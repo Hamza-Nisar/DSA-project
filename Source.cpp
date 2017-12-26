07047136e76f342e19dc23f6db39bcc8eda440c1
Hamza-Nisar <34794453+Hamza-Nisar@users.noreply.github.com>
2017-12-26 23:03:39 +0500

    Add files via upload
    N ary tree

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,6 @@
 #define N 50   //Max number of nodes
 #define MAX 50 //Max size of set
 using namespace std;
-int q = 0;
 class node{
 
 public:
@@ -27,30 +26,35 @@ public:
 	}
 };
 class Nary_tree{
-	node* root;
+	
 public:
+	node* root;
 	Nary_tree(){
 		root = NULL;
 	}
-	void Add_node(int a, node* t){
+	void Add_node(int a, node* t,node *par){
 		node* p = t;
 		if (root == NULL)
 		{
 			root = new node(a);
 			root->parent = root;
 		}
-		else
+		else if (root!=NULL)
 		{
+			if (p==NULL)
+			{
+				p = new node(a);
+				p->parent = par;
+				return;
+			}
 			for (int i = 0; i < N; i++)
 			{
 
 				if (p->child[i] == NULL){
-					p->child[i] = new node(a);
-					p->child[i]->parent = p;
-					return;
+					Add_node(a, p->child[i],p);
 				}
 			}
-			if (p->child[N] != NULL)
+			if (p->child[N - 1] != NULL)
 			{
 				int i = 0;
 				p = p->parent;
@@ -58,18 +62,43 @@ public:
 				{
 					i++;
 				}
-
-				Add_node(a, p->child[i]);
+				if (p->child[i] == NULL){
+					Add_node(a, p->child[i],p);
+					return;
+				}
 			}
+		}
+		else {
+			while (p->child[0] != NULL){
+				p = p->child[0];
+			}
+			Add_node(a, p->child[0],p);
 		}
 	}
 	void traverse(node * p){
-		cout << p->value;
-
-		for (int i = 0; i < N; i++)
+		if (p != NULL)
 		{
-			
+			for (int i = 0; i < N; i++){
+				
+				traverse(p->child[i]);
+				cout << " " << p->value << " -> ";
+			}
 		}
-
+		
 	}
 };
+void main(){
+	Nary_tree N1;
+	int A[1000];
+	for (int i = 0; i < 1000; i++)
+	{
+		A[i] = i + 1;
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+		int a = A[i];
+		N1.Add_node(a, N1.root, N1.root);
+	}
+	N1.traverse(N1.root);
+	system("pause");
+}
